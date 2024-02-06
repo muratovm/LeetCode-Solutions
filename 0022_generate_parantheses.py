6f6e06e26e5f02286cb87fbda5614f349bcde7fb
Michael Muratov <michael.muratov@mail.utoronto.ca>
2024-02-06 10:47:08 -0500

    Update 0022_generate_parantheses.py

--- 0022_generate_parantheses.py
+++ 0022_generate_parantheses.py
@@ -1,1 +1,24 @@
+class Solution:
+    
+    def __init__(self):
+        self.n = None
 
+    def recurse(self, left, right, net, string):
+        if left == self.n and right == self.n and net == 0:
+            return [string]
+
+        options = []
+        #add right parenthesis
+        if net > 0:
+            results = self.recurse(left, right+1, net-1, string+")")
+            options.extend(results)
+
+        #add left parenthesis
+        if net < self.n and left < self.n:
+            results = self.recurse(left+1, right, net+1, string+"(")
+            options.extend(results)
+        return options
+
+    def generateParenthesis(self, n):
+        self.n = n
+        return self.recurse(1,0,1,"(")
