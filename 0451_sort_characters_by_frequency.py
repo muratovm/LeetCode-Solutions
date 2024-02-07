048bd8e4b8871c9922a969f2e8e52218e9f9ca77
Michael Muratov <michael.muratov@mail.utoronto.ca>
2024-02-06 19:24:34 -0500

    Update 0451_sort_characters_by_frequency.py

--- 0451_sort_characters_by_frequency.py
+++ 0451_sort_characters_by_frequency.py
@@ -3,7 +3,4 @@ from collections import Counter
 class Solution:
     def frequencySort(self, s: str) -> str:
         counter = Counter(s)
-        result = ""
-        for char in sorted(counter, key=lambda x: counter[x], reverse=True):
-            result += char*counter[char]
-        return result
+        return "".join(char*counter[char] for char in sorted(counter, key=lambda x: counter[x], reverse=True))
